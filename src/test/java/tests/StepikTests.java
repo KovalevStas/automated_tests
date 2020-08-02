f4ce4cd2472408581050bb5ff1b15d6a143f1143
Станислав <stanislavkovalev.m@gmail.com>
2020-08-02 23:27:38 +0300

    Add joinAndExitCourseTest, AfterEach

--- src/test/java/tests/StepikTests.java
+++ src/test/java/tests/StepikTests.java
@@ -1,12 +1,15 @@
 package tests;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 class StepikTests {
 
@@ -21,8 +24,9 @@ class StepikTests {
         // Ввести пароль и нажать Enter
         $(byName("password")).setValue("pikabu123").pressEnter();
     }
+
     @Test
-    void authorizeTest() {
+    void checkProfileTest() {
 
         // Проверить наличие аватара пользователя
         $(".navbar__profile-img").isEnabled();
@@ -36,11 +40,45 @@ class StepikTests {
 
     @Test
     void searchTest(){
-
+        $(byText("Каталог")).click();
         //$(".st-course-filters").shouldHave(text("Математика")).click();
         //Вводим запрос в поле поиска
         $(".explore__search__input-wrapper input[placeholder = 'Поиск по каталогу']").setValue("Уравнения").pressEnter();
         //Проверяем результат поиска
         $(".course-pack").shouldHave(text("Уравнения"));
+    }
+
+    @Test
+    void joinAndExitCourseTest(){
+        //Найти курс и перейти на его страницу
+        $(byText("Машинное обучение и управление проектами в IT для преподавателей")).click();
+        //Записаться на курс
+        $(byText("Поступить на курс")).click();
+        //Дождаться загрузки страницы первого урока
+        $(".lesson-sidebar__course-title").shouldBe(visible);
+        //Открыть список меню пользователя и перейти в пункт "Мои курсы"
+        $(".navbar__profile-img").click();
+        $(".drop-down__body a",1).click();
+        //Проверить наличие курса в списке курсов пользователя
+        $(".course-widget__main-info a").shouldHave(text("Машинное обучение и управление проектами в IT для преподавателей"));
+        //Покинуть курс
+        $(".course-widget__menu").click();
+        $(byText("Покинуть")).click();
+        //Подтвердить действие
+        switchTo().alert().accept();
+        //Проверить, что у объекта курса пропали контролы
+        Assertions.assertEquals(true,$$("course-widget__menu-toggler").size() ==0);
+        //Обновить страницу
+        refresh();
+        //Проверить,что список курсов пустой
+        Assertions.assertEquals(true,$$(".course-widget__main-info").size() ==0);
+    }
+
+    @AfterEach
+    void exit(){
+        $(".navbar__profile-img").click();
+        $(".drop-down__body button",1).click();
+        switchTo().alert().accept();
+        closeWindow();
     }
 }
