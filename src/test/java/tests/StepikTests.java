d4dd40fb986ac155ababdffdaed5c2108abfff12
Станислав <stanislavkovalev.m@gmail.com>
2020-08-06 19:58:27 +0300

    refactor code

--- src/test/java/tests/StepikTests.java
+++ src/test/java/tests/StepikTests.java
@@ -1,12 +1,13 @@
 package tests;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class StepikTests {
@@ -21,98 +22,73 @@ class StepikTests {
         }
 
     @Test
-    void IncorrectAuthorizationTest() {
-        // Переход на страницу тестируемого сайта
+    void unSuccessfulAuthorizationTest() {
         open(url);
-        //Нажатие кнопки "Войти"
-        $(".navbar__auth_login").click();
-        // Ввести логин
+
+        $(".navbar__auth_login").click(); //Нажатие кнопки "Войти"
         $(byName("login")).setValue(login);
-        // Ввести пароль и нажать Enter
         $(byName("password")).setValue("123").pressEnter();
-        // Проверить наличие срообщения об ошибке
-        $("ul.sign-form__messages li[role='alert']").shouldBe(visible);
-        //$(".modal-dialog-top__close").click();
+
+        $("ul.sign-form__messages li[role='alert']").shouldBe(visible); // Проверить наличие срообщения об ошибке
     }
 
     @Test
     void checkProfileTest() {
-        // Переход на страницу тестируемого сайта
         open(url);
-        //Нажатие кнопки "Войти"
-        $(".navbar__auth_login").click();
-        // Ввести логин
+
+        $(".navbar__auth_login").click(); //Нажатие кнопки "Войти"
         $(byName("login")).setValue(login);
-        // Ввести пароль и нажать Enter
         $(byName("password")).setValue(password).pressEnter();
-        // Проверить наличие аватара пользователя
-        $(".navbar__profile-img").isEnabled();
-        //Вызов выпадающего меню
-        $(".navbar__profile-img").click();
-        //Нажатие на пункт "Профиль"
-        $(".drop-down__body a").click();
-        //Проверка отображения имени пользователя
-        $("img.navbar__profile-img").shouldBe(visible);
-        $(".profile-header-widget__name-wrapper h1").shouldHave(text("Тестовый пользователь"));
+
+        $(".navbar__profile-img").shouldBe(enabled).click(); // Проверить наличие аватара пользователя
+        $(".drop-down__body a").click(); //Нажатие на пункт "Профиль"
+        $(".profile-header-widget__name-wrapper h1").shouldHave(text("Тестовый пользователь")); //Проверка отображения имени пользователя
     }
 
     @Test
     void searchTest() {
-        // Переход на страницу тестируемого сайта
         open(url);
-        //Нажатие кнопки "Войти"
-        $(".navbar__auth_login").click();
-        // Ввести логин
+
+        $(".navbar__auth_login").click(); //Нажатие кнопки "Войти"
         $(byName("login")).setValue(login);
-        // Ввести пароль и нажать Enter
         $(byName("password")).setValue(password).pressEnter();
+
         $(byText("Каталог")).click();
-        //$(".st-course-filters").shouldHave(text("Математика")).click();
-        //Вводим запрос в поле поиска
-        $(".explore__search__input-wrapper input[placeholder = 'Поиск по каталогу']").setValue("Уравнения").pressEnter();
-        //Проверяем результат поиска
-        $(".course-pack").shouldHave(text("Уравнения"));
+        $(by("placeholder","Поиск по каталогу")).setValue("Уравнения").pressEnter(); //Вводим запрос в поле поиска
+        $(".course-pack").shouldHave(text("Уравнения")); //Проверяем результат поиска
     }
 
     @Test
     void joinAndExitCourseTest() {
-        // Переход на страницу тестируемого сайта
         open(url);
-        //Нажатие кнопки "Войти"
-        $(".navbar__auth_login").click();
-        // Ввести логин
+
+        $(".navbar__auth_login").click(); //Нажатие кнопки "Войти"
         $(byName("login")).setValue(login);
-        // Ввести пароль и нажать Enter
         $(byName("password")).setValue(password).pressEnter();
-        //Найти курс и перейти на его страницу
-        $(byText("Машинное обучение и управление проектами в IT для преподавателей")).click();
-        //Записаться на курс
-        $(byText("Поступить на курс")).click();
-        //Дождаться загрузки страницы первого урока
-        $(".lesson-sidebar__course-title").shouldBe(visible);
+
+
+        $(byText("Машинное обучение и управление проектами в IT для преподавателей")).click(); //Найти курс и перейти на его страницу
+        $(byText("Поступить на курс")).click();  //Записаться на курс
+        $(".lesson-sidebar__course-title").shouldBe(visible); //Дождаться загрузки страницы первого урока
         //Открыть список меню пользователя и перейти в пункт "Мои курсы"
         $(".navbar__profile-img").click();
         $(".drop-down__body a", 1).click();
-        //Проверить наличие курса в списке курсов пользователя
-        $(".course-widget__main-info a").shouldHave(text("Машинное обучение и управление проектами в IT для преподавателей"));
+
+        $(".course-widget__main-info a")
+                .shouldHave(text("Машинное обучение и управление проектами в IT для преподавателей")); //Проверить наличие курса в списке курсов пользователя
+
         //Покинуть курс
         $(".course-widget__menu").click();
         $(byText("Покинуть")).click();
-        //Подтвердить действие
-        switchTo().alert().accept();
-        //Проверить, что у объекта курса пропали контролы
-        Assertions.assertEquals(0, $$("course-widget__menu-toggler").size());
-        //Обновить страницу
-        refresh();
-        //Проверить,что список курсов пустой
-        Assertions.assertEquals(0, $$(".course-widget__main-info").size());
+        switchTo().alert().accept(); //Подтвердить действие
+
+        $$(".course-widget__menu-toggler").shouldHave(size(0)); //Проверить, что у объекта курса пропали контролы
+        refresh(); //Обновить страницу
+        $$(".course-widget__main-info").shouldHave(size(0)); //Проверить,что список курсов пустой
     }
 
     @AfterEach
     void exit() {
-        /*$(".navbar__profile-img").click();
-        $(".drop-down__body button", 1).click();
-        switchTo().alert().accept();*/
-        closeWindow();
+        closeWebDriver();
     }
 }
